d19578507d20fd38d33b13b099ca67cf1045aec3
Mohammad Norouzzadegan <Norouzzadegan@gmail.com>
2024-02-15 16:15:14 +0330

    update __main__

--- src/surena/__main__.py
+++ src/surena/__main__.py
@@ -1,9 +1,14 @@
 import click
 
-from surena.cli.commands import is_docker_host
+from surena.cli.commands import get_docker_host, is_docker_host
 
 
-@click.group(commands={is_docker_host.name: is_docker_host})
+@click.group(
+    commands={
+        is_docker_host.name: is_docker_host,
+        get_docker_host.name: get_docker_host,
+    }
+)
 def surena() -> None:
     """
     Surena is a security tool designed to access the Docker host
